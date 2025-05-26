dc2df0f232e8934994f6aa44e87768f175a268b0
Jaden Szewczak <jaden.szewczak@wakefern.com>
2025-05-26 16:12:46 -0400

    Add SSL support for production database connection

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -5,10 +5,18 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    })
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: process.env.DB_HOST || 'localhost',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
